394612f5c2a1d9c01d028b0418fd84297e79318e
Henry YP Ho <me@h3nr7.com>
2024-06-21 11:56:03 +0100

    close to working radar chart

--- frontend/src/components/RadarChart/RadarChart.tsx
+++ frontend/src/components/RadarChart/RadarChart.tsx
@@ -2,7 +2,8 @@ import { PropsWithChildren, useEffect, useMemo, useRef, useState } from "react"
 import { SVG } from '@svgdotjs/svg.js'
 
 export interface RadarChartData {
-
+  label: string
+  value: number
 }
 
 export interface RadarChartProps {
@@ -10,45 +11,89 @@ export interface RadarChartProps {
   step?: number
   min?: number
   max?: number
+  data: RadarChartData[]
 }
 
 export function RadarChart({
   diameter = 200,
   step = 1,
-  max = 5
+  max = 5,
+  data = [
+    {
+      label: 'stamina',
+      value: 1
+    },
+    {
+      label: 'agility',
+      value: 4
+    },
+    {
+      label: 'flexibility',
+      value: 4
+    },
+    {
+      label: 'plusmimus',
+      value: 3
+    },
+    {
+      label: 'plusmimus',
+      value: 5
+    }
+  ]
 }:PropsWithChildren<RadarChartProps>) {
 
-  const [data, setData] = useState<RadarChartData[]>([''] as RadarChartData[])
   const ref= useRef<HTMLDivElement>(null)
-  const svgContainer = useMemo(() => SVG().width(diameter).height(diameter), [data]);
+  const svgContainer = useMemo(() => SVG().width(diameter).height(diameter), []);
 
-  const { circles } = useMemo(() => {
+  const { elements } = useMemo(() => {
     if(!svgContainer || !data?.length) return {}
-    let circles = [];
+    let elements = [];
     const radius = diameter / 2
     const count = Math.ceil(max/step)
 
     for(let i = 1; i < count+1; i++) {
       const offset = -radius*i/count + radius
-      circles.push(SVG().circle(diameter*i/count).fill('#666666').move(offset, offset).opacity(0.5))
+      elements.push(SVG().circle(diameter*i/count).fill('#666666').move(offset, offset).opacity(0.5))
     }
+  
+    const len = data.length
+    const pi = 3.1415
+
+    const polyStr = useMemo(() => {
+
+      const pos = data.map((d, i) => {
+        const offset = radius
+        const hyp = d.value/max * radius
+
+        const x = hyp * Math.cos(i/len * 2 * pi) + offset
+        const y = hyp * Math.sin(i/len * 2 * pi) + offset
+        
+        return [x, y]
+      })
+
+      return pos.map(p => `${p[0]},${p[1]}`).join(' ')
+
+    },[data])
+
+    elements.push(SVG().polygon(polyStr).fill('#ffe600').opacity(0.3))
+
 
     return {
-      circles
+      elements
     }
   }, [data, svgContainer, ref])
 
   useEffect(() => {
-    console.log('lala', circles)
-    if(!circles?.length || !ref?.current) return
-    circles.map(c => svgContainer.add(c))
+    console.log('lala', elements)
+    if(!elements?.length || !ref?.current) return
+    elements.map(c => svgContainer.add(c))
     
     svgContainer.addTo(ref.current);
 
     return () => {
       svgContainer.clear()
     }
-  }, [circles, svgContainer])
+  }, [elements, svgContainer])
 
 
 
